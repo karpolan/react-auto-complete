dba1fdbb2281ffafc078cabce3f88e6607d842a0
Anton Karpenko <karpolan@karpolan.com>
2020-06-13 14:59:35 +0200

    feat: AutoCompleteClass prototype

--- src/components/AutoComplete/AutoCompleteClass/AutoCompleteClass.js
+++ src/components/AutoComplete/AutoCompleteClass/AutoCompleteClass.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Renders the text Input with autocomplete suggestions List
+ * @class AutoCompleteClass
+ * @author Anton Karpenko
+ */
 class AutoCompleteClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '', // User input
+      showSuggestions: false, // the Suggestions list is rendered when true
+    };
+  }
+
+  onChange = (event) => {
+    const { value } = this.state;
+    const newValue = event.currentTarget.value;
+    if (newValue === value) {
+      return; // Nothing was changed...
+    }
+
+    this.setState({
+      value: newValue,
+      showSuggestions: Boolean(newValue), // true when non-empty
+    });
+  };
+
+  renderSuggestions() {
+    return <div>Suggestions list here...</div>;
+  }
+
   render() {
+    const { value, showSuggestions } = this.state;
     return (
       <>
-        <div>AutoCompleteClass is under construction</div>
+        <input type="text" value={value} onChange={this.onChange} />
+        {showSuggestions && this.renderSuggestions()}
       </>
     );
   }
